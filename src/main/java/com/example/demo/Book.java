058644b32075950d908d3fd5aea749d71ca6dcea
jkjackson <tecnico_jackson@hotmail.com>
2020-06-23 00:11:27 -0300

    projeto criado

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class Book {
+	
+	Long idnumero;
+	String nome;
+	String ISBN;
+	public Long getIdnumero() {
+		return idnumero;
+	}
+	public void setIdnumero(Long idnumero) {
+		this.idnumero = idnumero;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+}
